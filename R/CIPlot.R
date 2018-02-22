04f58bf3bc363ec5ec46ec2b7af46942e2d5a659
Eddie Polanco <eddiepolanco@chemeng.utah.edu>
2018-02-21 19:47:00 -0700

    Initilizing Repository

---
+++ R/CIPlot.R
@@ -0,0 +1,12 @@
+#' @title Generates a confidence limit
+#'
+#' @param samples Specify how many samples to run
+#' @param sample_size the number of observations in each sample
+#' @param mean population mean
+#' @param sd population standard deviation
+#'
+#' @export
+#'
+# shift + cmd + d
+# or roxygen::roxygenise()
+
---
+++ R/SamplingDist.R
@@ -0,0 +1,11 @@
+#' @title Generates a poor man's sampling distribution
+#'
+#' @param samples Specify how many samples to run
+#' @param sample_size the number of observations in each sample
+#' @param mean population mean
+#' @param sd population standard deviation
+#'
+#' @export
+#'
+# shift + cmd + d
+# or roxygen::roxygenise()
